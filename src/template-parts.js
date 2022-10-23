d11ef2f9f29a19c1fcac25485826ec94b21fe8ed
Dmitry Iv <df.creative@gmail.com>
2022-10-23 05:40:56 -0400

    Fix namespace

--- src/template-parts.js
+++ src/template-parts.js
@@ -60,7 +60,7 @@ export class AttributeTemplatePart extends TemplatePart {
     } else element.setAttributeNS(attr.namespaceURI, attr.name, parts.join(''));
   }
   get booleanValue() {
-    return this.setter.element.hasAttributeNS(this.setter.attr.name);
+    return this.setter.element.hasAttributeNS(this.attributeNamespace, this.setter.attr.name);
   }
   set booleanValue(value) {
     if (this.setter.parts.length === 1) this.value = value ? '' : null;
